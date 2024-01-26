11c330b69d04825a355e4ff6dafdffbaa3d52b1d
Marcos <146812640+MarcosGorriaran@users.noreply.github.com>
2024-01-26 12:14:17 +0100

    Time to start making the interface based on what the teachers provided us.

---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+//Declaración de constantes.
+const MAX_INTENTOS = 10;
+const MAX_COMBI_COLORES = 4;
+const COLORS = ['white', 'blue', 'green', 'violet', 'yellow', 'red', 'orange', 'cyan'];
+const GREY = "grey";
+const WHITE = "white";
+const BLACK = "black";
+
+
+//Declaración de variables globales.
+let master = PickSequence(COLORS, MAX_COMBI_COLORES);
+let userCombi = [];
+var intento = 0;
+var aciertos = 0;
+let selected 
+function init() {
+    //1. Genera el código random del master
+    document.getElementById("Result").innerHTML=ROW_RESULT;
+    //2. Crea todas las filas según el número de intentos.
+}
+
+function AddSelectFunction(selections){
+    for(let i = 0; i<)
+}
+
+/* Llamaremos a esta función desde el botón HTML de la página para comprobar la propuesta de combinación que nos ha
+introducido el usuario.
+Informamos al usuario del resultado y del número de intentos que lleva*/
+function Comprobar() {
+
+}
+
+/** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
+function añadeColor(color) {
+   
+}
+
+function Selected(){
+    
+}
+/** Template con el código HTML que corresponde a cada fila de juego/intento. */
+const ROW_RESULT = `<div class="rowResult w100 flex wrap">
+<div class="rowUserCombi w75 flex wrap">
+   <div class="w25">
+       <div class="celUserCombi flex" onclick="selected(1)"></div>
+   </div>
+   <div class="w25">
+       <div class="celUserCombi flex" onclick="selected(2)"></div>
+   </div>
+   <div class="w25">
+       <div class="celUserCombi flex" onclick="selected(3)"></div>
+   </div>
+   <div class="w25">
+       <div class="celUserCombi flex" onclick="selected(4)"></div>
+   </div>
+</div>
+<div class="rowCercleResult w25 flex wrap center">
+   <div class="w40 h40">
+        <div class="cercleResult flex"></div>
+   </div>
+   <div class="w40 h40">
+       <div class="cercleResult flex"></div>
+   </div>
+   <div class="w40 h40">
+       <div class="cercleResult flex"></div>
+   </div>
+   <div class="w40 h40">
+       <div class="cercleResult flex"></div>
+   </div>
+<div>
+</div>`;
